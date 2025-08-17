16f8f44663fc881e5a37bc1ef088b2823f5e59a4
Joao Gabriel <jgafarias1@gmail.com>
2025-08-17 20:35:04 -0300

    New functions\n  - Text function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Conversor de PDF"
     page.window.width = 500
-    page.window.height = 500
+    page.window.height = 300
 
     # Função chamada quando o usuário escolhe um arquivo
     def on_file_selected(e: ft.FilePickerResultEvent):
@@ -14,20 +14,41 @@ def main(page: ft.Page):
             selected_file.value = "Nenhum arquivo selecionado"
         page.update()
 
-    # Cria o FilePicker e adiciona ao overlay da página
-    file_picker = ft.FilePicker(on_result=on_file_selected)
-    page.overlay.append(file_picker)
-    
-    # Cria o FilePicker e adiciona ao overlay da página
     file_picker = ft.FilePicker(on_result=on_file_selected)
     page.overlay.append(file_picker)
 
+    file_picker = ft.FilePicker(on_result=on_file_selected)
+    page.overlay.append(file_picker)
+
+
+    def generate_pdf(e):
+        page.open(ft.SnackBar(ft.Text("PDF gerado com sucesso!", weight=ft.FontWeight.BOLD), bgcolor=ft.Colors.GREEN))
 
     title = ft.Text(
-        "Conversor PDF",      # texto
-        size=30,              # tamanho da fonte
-        weight=ft.FontWeight.BOLD,# negrito        
+        "Conversor PDF",
+        size=30,
+        weight=ft.FontWeight.BOLD, 
         text_align=ft.MainAxisAlignment.CENTER 
+    )
+    
+    def texto(text_view: str):
+        return ft.Text(text_view, size=15, color=ft.Colors.with_opacity(0.3, ft.Colors.WHITE))
+    
+    save_btn = ft.ElevatedButton(
+        "GERAR",
+        height=50,
+        width=110,
+        icon=ft.Icons.PICTURE_AS_PDF_OUTLINED,
+        on_click=generate_pdf,
+        bgcolor=ft.Colors.with_opacity(0.5, ft.Colors.RED),
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=10),
+            overlay_color=ft.Colors.RED,
+            text_style=ft.TextStyle(
+                weight=ft.FontWeight.BOLD,
+                color=ft.Colors.BLACK,
+            )
+        )
     )
     
     # Texto que vai mostrar o arquivo selecionado
@@ -46,7 +67,11 @@ def main(page: ft.Page):
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         controls=[
             title,
+            ft.Divider(),
+            texto("Selecione o arquivo para converter"),
             ft.Row([selected_file, picker_button], alignment=ft.MainAxisAlignment.CENTER),
+            ft.Container(height=10),
+            save_btn,
         ],
     ))
 
