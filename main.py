c6725f05a21d997dad25768f079b67fa0830801d
Joao Gabriel <jgafarias1@gmail.com>
2025-08-17 18:15:14 -0300

    Better layout

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main(page: ft.Page):
     def on_file_selected(e: ft.FilePickerResultEvent):
         if e.files:
             file_path = e.files[0].path
-            selected_file.value = f"Arquivo selecionado: {file_path}"
+            selected_file.value = f"{file_path}"
         else:
             selected_file.value = "Nenhum arquivo selecionado"
         page.update()
@@ -17,9 +17,21 @@ def main(page: ft.Page):
     # Cria o FilePicker e adiciona ao overlay da página
     file_picker = ft.FilePicker(on_result=on_file_selected)
     page.overlay.append(file_picker)
+    
+    # Cria o FilePicker e adiciona ao overlay da página
+    file_picker = ft.FilePicker(on_result=on_file_selected)
+    page.overlay.append(file_picker)
 
+
+    title = ft.Text(
+        "Conversor PDF",      # texto
+        size=30,              # tamanho da fonte
+        weight=ft.FontWeight.BOLD,# negrito        
+        text_align=ft.MainAxisAlignment.CENTER 
+    )
+    
     # Texto que vai mostrar o arquivo selecionado
-    selected_file = ft.Text("Arquivo ()")
+    selected_file = ft.TextField(hint_text="Nenhum arquivo selecionado")
 
     # Botão que abre o FilePicker
     picker_button = ft.IconButton(
@@ -29,6 +41,13 @@ def main(page: ft.Page):
     )
 
     # Coloca os widgets dentro de uma coluna
-    page.add(ft.Row([selected_file, picker_button]))
+    page.add(ft.Column(
+        alignment=ft.MainAxisAlignment.CENTER,       # eixo principal (vertical)
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[
+            title,
+            ft.Row([selected_file, picker_button], alignment=ft.MainAxisAlignment.CENTER),
+        ],
+    ))
 
 ft.app(target=main)
